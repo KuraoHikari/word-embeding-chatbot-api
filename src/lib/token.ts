5ebe70f4d2efbb026a38cf42b415ba3fb05caa78
KuraoHikari <dewaindra705@gmail.com>
2025-05-10 12:50:23 +0800

    chore: - add auth middleware to protect routes for public and private chatbots - add token to the request object for public and private chatbots

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -11,3 +11,22 @@ export async function generateAccessToken(userId: number): Promise<string> {
   const accessToken = await sign(payload, env.ACCESS_TOKEN_SECRET);
   return accessToken;
 }
+
+// Function to generate a public access token
+export async function generatePublicAccessToken(userId: number): Promise<string> {
+  const payload = {
+    sub: userId,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * Number(env.ACCESS_TOKEN_EXPIRES_IN_PUBLIC),
+  };
+  const accessToken = await sign(payload, env.ACCESS_TOKEN_SECRET_PUBLIC);
+  return accessToken;
+}
+
+export async function generatePublicContactAccessToken(cotactId: number): Promise<string> {
+  const payload = {
+    sub: cotactId,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * Number(env.ACCESS_TOKEN_EXPIRES_IN_PUBLIC),
+  };
+  const accessToken = await sign(payload, env.ACCESS_TOKEN_SECRET_PUBLIC);
+  return accessToken;
+}
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -20,7 +20,28 @@ export const authMiddleware: MiddlewareHandler = async (c, next) => {
     const payload = await verify(token, env.ACCESS_TOKEN_SECRET);
     c.set("userId", payload.sub); // Simpan user ID di context
     await next(); // Lanjut ke handler berikutnya
-  }catch {
+  }
+  catch {
+    return c.json({ message: HttpStatusPhrases.UNAUTHORIZED }, HttpStatusCodes.UNAUTHORIZED);
+  }
+};
+
+export const authMiddlewarePublic: MiddlewareHandler = async (c, next) => {
+  const authHeader = c.req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return c.json({ message: HttpStatusPhrases.UNAUTHORIZED }, HttpStatusCodes.UNAUTHORIZED);
+  }
+
+  const token = authHeader.slice(7);
+
+  try {
+    const payload = await verify(token, env.ACCESS_TOKEN_SECRET_PUBLIC);
+
+    c.set("userId", payload.sub); // Simpan user ID di context
+    await next(); // Lanjut ke handler berikutnya
+  }
+  catch {
     return c.json({ message: HttpStatusPhrases.UNAUTHORIZED }, HttpStatusCodes.UNAUTHORIZED);
   }
 };
