aaf1c024fb7a6e74dfe790c504c93a2dec6242c8
KuraoHikari <dewaindra705@gmail.com>
2025-05-19 14:50:52 +0800

    feat: - add: ai completion for messages base on context

--- src/routes/messages/messages.handler.ts
+++ src/routes/messages/messages.handler.ts
@@ -1,4 +1,5 @@
 import { eq } from "drizzle-orm";
+import { OpenAI } from "openai";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import * as HttpStatusPhrases from "stoker/http-status-phrases";
 
@@ -10,6 +11,10 @@ import env from "@/env";
 import { sendQueryRequestWithoutRetry } from "@/lib/send-query-request-without-retry";
 
 import type { CreateRoute } from "./messages.routes";
+
+const openai = new OpenAI({
+  apiKey: env.OPEN_AI_API_KEY, // This is also the default, can be omitted
+});
 
 export const create: AppRouteHandler<CreateRoute> = async (c) => {
   const contactId = c.get("contactId");
@@ -54,7 +59,7 @@ export const create: AppRouteHandler<CreateRoute> = async (c) => {
       chatbotId: String(chatbot.id),
       modelType: chatbot.embedingModel,
       pdfTitle: chatbot.pdfTitle,
-      topK: 10,
+      topK: 2,
       similarityThreshold: 0.8,
     },
     env.API_PASSWORD,
@@ -62,7 +67,35 @@ export const create: AppRouteHandler<CreateRoute> = async (c) => {
   console.log("🚀 ~ constcreate:AppRouteHandler<CreateRoute>= ~ response:", response);
 
   const results = await response.json();
+
+  if (!results) {
+    return c.json(
+      { message: HttpStatusPhrases.INTERNAL_SERVER_ERROR },
+      HttpStatusCodes.INTERNAL_SERVER_ERROR,
+    );
+  }
+
+  const prompt = "AI assistant is a professional and polite customer service. \nThe traits of AI include expert knowledge, helpfulness, cleverness, and articulateness. \nAI assistant provides clear, concise, and friendly responses without repeating unnecessary information or phrases such as \"Berdasarkan informasi yang diberikan sebelumnya.\", \"dalam konteks yang diberikan.\", \"dalam konteks yang tersedia.\". \nAI is a well-behaved and well-mannered individual. \nAI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user. \nAI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation. \nAI assistant make answer using Indonesian Language. \nAI assistant avoids sounding repetitive and ensures responses sound natural and tailored to each question. \nIf the context does not provide the answer to question, the AI assistant will say, \"Mohon Maaf, tapi saya tidak dapat menjawab pertanyaan tersebut saat ini.\". \nSTART CONTEXT BLOCK \n{{context}} \nEND OF CONTEXT BLOCK \nAI assistant will take into account any CONTEXT BLOCK that is provided in a conversation. \nIf the context does not provide the answer to question, the AI assistant will say, \"Mohon Maaf, tapi saya tidak dapat menjawab pertanyaan tersebut saat ini.\". \nAI assistant will not apologize for previous responses, but instead will indicated new information was gained. \nAI assistant will not invent anything that is not drawn directly from the context.";
+
+  const promtFromDb = {
+    role: "system" as const,
+    content: prompt.replace("{{context}}", results.results),
+  };
+
+  const askAi = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: [
+      promtFromDb,
+      {
+        role: "user" as const,
+        content: message.text,
+      },
+    ],
+  });
   return c.json({
-    ...results,
+    ...askAi.choices[0].message,
+    results,
+
+    promtFromDb,
   });
 };
